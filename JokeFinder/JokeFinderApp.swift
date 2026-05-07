975c79daffb40cd11e716900e844085f46bd3b04
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-05-07 12:28:50 -0400

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Alexander Saadia on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            JokeView()
+        }
+    }
+}
---
+++ JokeFinder/View/JokeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeFinder
+//
+//  Created by Alexander Saadia on 07/05/26.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    JokeView()
+}
