cbf4d4bc0b07d09540d79908ed6f8fcc979e67eb
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-05-07 21:22:02 -0400

    Added a button so that we can load a new joke if desired.

--- JokeFinder/View/JokeView.swift
+++ JokeFinder/View/JokeView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+internal import Combine
 
 struct JokeView: View {
     
@@ -6,6 +7,18 @@ struct JokeView: View {
     
     // Create the view model (temporarily show the default joke)
     @State var viewModel = JokeViewModel()
+    
+    // Controls punchline visibility
+    @State var punchlineOpacity = 0.0
+    
+    // Controls button visibility
+    @State var buttonOpacity = 0.0
+    
+    // Starts a timer to wait on revealing punchline
+    @State var punchlineTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    
+    // Starts a timer to wait on revealing button to get new joke
+    @State var buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     
     // MARK: Computed properties
     var body: some View {
@@ -19,10 +32,55 @@ struct JokeView: View {
                         .padding(.bottom, 100)
                     
                     Text(currentJoke.punchline ?? "")
+                        .opacity(punchlineOpacity)
+                        .onReceive(punchlineTimer) { _ in
+                            
+                            withAnimation {
+                                punchlineOpacity = 1.0
+                            }
+                            
+                            // Stop the timer
+                            punchlineTimer.upstream.connect().cancel()
+                        }
  
                 }
                 .font(.title)
                 .multilineTextAlignment(.center)
+                
+                Button {
+                 
+                    // Hide punchline and button
+                    withAnimation {
+                        viewModel.currentJoke = nil
+                        punchlineOpacity = 0.0
+                        buttonOpacity = 0.0
+                    }
+                                        
+                    // Get a new joke
+                    Task {
+                        await viewModel.fetchJoke()
+                    }
+                    
+                    // Restart timers
+                    punchlineTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+                    buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+                    
+                } label: {
+                 
+                    Text("New Joke")
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .opacity(buttonOpacity)
+                .onReceive(buttonTimer) { _ in
+                    
+                    withAnimation {
+                        buttonOpacity = 1.0
+                    }
+                    
+                    // Stop the timer
+                    buttonTimer.upstream.connect().cancel()
+                }
                 
             }
             
